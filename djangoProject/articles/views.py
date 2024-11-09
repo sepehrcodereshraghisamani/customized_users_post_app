fdc80851d405fbf45bc865eb148dd1933df91167
sepehrcodereshraghisamani <sepehr.esh.sa.1999@gmail.com>
2024-11-09 07:09:44 -0800

    listing articles

--- djangoProject/articles/views.py
+++ djangoProject/articles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Article
+from django.views.generic import ListView
 
 # Create your views here.
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'articles/article_list.html'
+
--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     #path('',TemplateView.as_view(template_name='home.html'),name='home'),
     path('',include('pages.urls')),
+    path('articles/',include('articles.urls')),
 ]
