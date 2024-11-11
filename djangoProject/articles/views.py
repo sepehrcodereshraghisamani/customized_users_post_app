4a78e68a9312c46be857b6b6832cf575582cc0b8
sepehrcodereshraghisamani <sepehr.esh.sa.1999@gmail.com>
2024-11-11 08:38:46 -0800

    changing the Article_Create_view class to inherit Login_RequiredMixins

--- djangoProject/articles/views.py
+++ djangoProject/articles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Article
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView,CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
@@ -25,8 +26,12 @@ class ArticleDeleteView(DeleteView):
     template_name = "articles/article_delete.html"
     success_url = reverse_lazy('article_list')
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin , CreateView):
     model = Article
-    fields = ("title", "body","author")
+    fields = ("title", "body")
     template_name = "articles/article_create.html"
     success_url = reverse_lazy('article_list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
