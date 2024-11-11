1199841d68a2f6a53691595574f08f6824515b76
sepehrcodereshraghisamani <sepehr.esh.sa.1999@gmail.com>
2024-11-11 11:21:09 -0800

    using LoginRequiredMixin and UserPassesTestMixin in articles views

--- djangoProject/articles/views.py
+++ djangoProject/articles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Article
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView,CreateView
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
 
 # Create your views here.
@@ -15,16 +15,22 @@ class ArticleDetailView(DetailView):
     model = Article
     template_name = 'articles/article_detail.html'
 
-class ArticleEditView(UpdateView):
+class ArticleEditView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     model = Article
     fields = ("title", "body")
     template_name = "articles/article_edit.html"
     success_url = reverse_lazy('article_list')
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.author
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     model = Article
     template_name = "articles/article_delete.html"
     success_url = reverse_lazy('article_list')
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.author
 
 class ArticleCreateView(LoginRequiredMixin , CreateView):
     model = Article
