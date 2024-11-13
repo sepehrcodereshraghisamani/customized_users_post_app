128ba6aeeb5e72eafe1005170ba9b986f42d3779
sepehrcodereshraghisamani <sepehr.esh.sa.1999@gmail.com>
2024-11-13 07:58:06 -0800

    adding forms to create comment

--- djangoProject/articles/views.py
+++ djangoProject/articles/views.py
@@ -3,6 +3,10 @@ from .models import Article
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView,CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.views.generic.edit import FormMixin
+from django.forms import forms
+from .forms import CommentForm
+
 
 
 # Create your views here.
@@ -11,9 +15,29 @@ class ArticleListView(ListView):
     model = Article
     template_name = 'articles/article_list.html'
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(FormMixin,DetailView):
     model = Article
     template_name = 'articles/article_detail.html'
+    form_class = CommentForm
+    def get_success_url(self):
+        return reverse_lazy('article_detail',kwargs={'pk':self.object.pk})
+
+    def get_context_data(self, **kwargs):
+        context = super(ArticleDetailView, self).get_context_data(**kwargs)
+        context['form'] = CommentForm(initial={'article':self.object,'writer':self.request.user})
+        return context
+
+    def post(self, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            pass
+
+    def form_valid(self, form):
+        form.save()
+        return super(ArticleDetailView, self).form_valid(form)
 
 class ArticleEditView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     model = Article
@@ -41,3 +65,6 @@ class ArticleCreateView(LoginRequiredMixin , CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+
